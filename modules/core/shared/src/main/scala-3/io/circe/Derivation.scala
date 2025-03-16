travisbrown/jfc

commit 1303b401188fa99f48c1a7f0aa1869f5681f01ae

Derivation for fat structures (#2341)
* derivation for products/enums bigger than 32 fields

* fix singleton derivation

* add test and clean

* fmt and minor fix

* fmt scala3

* add common tests for long structures

modules/core/shared/src/main/scala-3/io/circe/derivation/package.scala
@@ -21,9 +21,9 @@ import scala.deriving.Mirror
 import io.circe.{ Codec, Decoder, Encoder }
 
 private[circe] inline final def summonLabels[T <: Tuple]: List[String] =
-  inline erasedValue[T] match
-    case _: EmptyTuple => Nil
-    case _: (t *: ts)  => constValue[t].asInstanceOf[String] :: summonLabels[ts]
+  loopUnrolledNoArg[String, T](
+    constString
+  )
 
 @deprecated("Use summonEncoders(derivingForSum: Boolean) instead", "0.14.7")
 private[circe] inline final def summonEncoders[T <: Tuple](using Configuration): List[Encoder[_]] =
@@ -32,9 +32,9 @@ private[circe] inline final def summonEncoders[T <: Tuple](using Configuration):
 private[circe] inline final def summonEncoders[T <: Tuple](inline derivingForSum: Boolean)(using
   Configuration
 ): List[Encoder[_]] =
-  inline erasedValue[T] match
-    case _: EmptyTuple => Nil
-    case _: (t *: ts)  => summonEncoder[t](derivingForSum) :: summonEncoders[ts](derivingForSum)
+  loopUnrolledNoArg[Encoder[_], T](
+    inline if (derivingForSum) new EncoderDeriveSum else new EncoderNotDeriveSum
+  )
 
 @deprecated("Use summonEncoder(derivingForSum: Boolean) instead", "0.14.7")
 private[circe] inline final def summonEncoder[A](using Configuration): Encoder[A] =
@@ -55,9 +55,9 @@ private[circe] inline final def summonDecoders[T <: Tuple](using Configuration):
 private[circe] inline final def summonDecoders[T <: Tuple](inline derivingForSum: Boolean)(using
   Configuration
 ): List[Decoder[_]] =
-  inline erasedValue[T] match
-    case _: EmptyTuple => Nil
-    case _: (t *: ts)  => summonDecoder[t](derivingForSum) :: summonDecoders[ts](derivingForSum)
+  loopUnrolledNoArg[Decoder[_], T](
+    inline if (derivingForSum) new DecoderDeriveSum else new DecoderNotDeriveSum
+  )
 
 @deprecated("Use summonDecoder(derivingForSum: Boolean) instead", "0.14.7")
 private[circe] inline final def summonDecoder[A](using Configuration): Decoder[A] =
@@ -72,10 +72,46 @@ private[circe] inline final def summonDecoder[A](inline derivingForSum: Boolean)
   }
 
 private[circe] inline def summonSingletonCases[T <: Tuple, A](inline typeName: Any): List[A] =
+  loopUnrolled[A, Any, T](new SummonSingleton[A], typeName)
+
+private[circe] inline final def loopUnrolled[A, Arg, T <: Tuple](f: Inliner[A, Arg], inline arg: Arg): List[A] =
   inline erasedValue[T] match
     case _: EmptyTuple => Nil
-    case _: (h *: t) =>
-      inline summonInline[Mirror.Of[h]] match
-        case m: Mirror.Singleton => m.fromProduct(EmptyTuple).asInstanceOf[A] :: summonSingletonCases[t, A](typeName)
-        case m: Mirror =>
-          error("Enum " + codeOf(typeName) + " contains non singleton case " + codeOf(constValue[m.MirroredLabel]))
+    case _: (h1 *: h2 *: h3 *: h4 *: h5 *: h6 *: h7 *: h8 *: h9 *: h10 *: h11 *: h12 *: h13 *: h14 *: h15 *: h16 *:
+          ts) =>
+      f[h1](arg) :: f[h2](arg) :: f[h3](arg) :: f[h4](arg) :: f[h5](arg) :: f[h6](arg) :: f[h7](arg) :: f[h8](arg)
+        :: f[h9](arg) :: f[h10](arg) :: f[h11](arg) :: f[h12](arg) :: f[h13](arg) :: f[h14](arg) :: f[h15](arg) :: f[
+          h16
+        ](arg)
+        :: loopUnrolled[A, Arg, ts](f, arg)
+    case _: (h1 *: h2 *: h3 *: h4 *: ts) =>
+      f[h1](arg) :: f[h2](arg) :: f[h3](arg) :: f[h4](arg) :: loopUnrolled[A, Arg, ts](f, arg)
+    case _: (h *: ts) => f[h](arg) :: loopUnrolled[A, Arg, ts](f, arg)
+
+private[circe] inline def loopUnrolledNoArg[A, T <: Tuple](f: Inliner[A, Unit]): List[A] =
+  loopUnrolled[A, Unit, T](f, ())
+
+private[circe] trait Inliner[A, Arg]:
+  inline def apply[T](inline arg: Arg): A
+
+private[circe] object constString extends Inliner[String, Unit]:
+  inline def apply[T](inline arg: Unit): String = constValue[T].asInstanceOf[String]
+
+private[circe] class EncoderDeriveSum(using config: Configuration) extends Inliner[Encoder[_], Unit]:
+  inline def apply[T](inline arg: Unit): Encoder[?] = summonEncoder[T](true)
+
+private[circe] class EncoderNotDeriveSum(using config: Configuration) extends Inliner[Encoder[_], Unit]:
+  inline def apply[T](inline arg: Unit): Encoder[?] = summonEncoder[T](false)
+
+private[circe] class DecoderDeriveSum(using Configuration) extends Inliner[Decoder[_], Unit]:
+  inline def apply[T](inline arg: Unit): Decoder[?] = summonDecoder[T](true)
+
+private[circe] class DecoderNotDeriveSum(using Configuration) extends Inliner[Decoder[_], Unit]:
+  inline def apply[T](inline arg: Unit): Decoder[?] = summonDecoder[T](false)
+
+private[circe] class SummonSingleton[A] extends Inliner[A, Any]:
+  inline def apply[T](inline typeName: Any): A =
+    inline summonInline[Mirror.Of[T]] match
+      case m: Mirror.Singleton => m.fromProduct(EmptyTuple).asInstanceOf[A]
+      case m: Mirror =>
+        error("Enum " + codeOf(typeName) + " contains non singleton case " + codeOf(constValue[m.MirroredLabel]))

modules/tests/shared/src/test/scala-3/io/circe/DerivesSuite.scala
@@ -223,6 +223,207 @@ object DerivesSuite {
     given Eq[Outer] = Eq.fromUniversalEquals
     given Arbitrary[Outer] =
       Arbitrary(Gen.option(Arbitrary.arbitrary[String].map(Inner.apply)).map(Outer.apply))
+
+  case class LongClass(
+    v1: String,
+    v2: String,
+    v3: String,
+    v4: String,
+    v5: String,
+    v6: String,
+    v7: String,
+    v8: String,
+    v9: String,
+    v10: String,
+    v11: String,
+    v12: String,
+    v13: String,
+    v14: String,
+    v15: String,
+    v16: String,
+    v17: String,
+    v18: String,
+    v19: String,
+    v20: String,
+    v21: String,
+    v22: String,
+    v23: String,
+    v24: String,
+    v25: String,
+    v26: String,
+    v27: String,
+    v28: String,
+    v29: String,
+    v30: String,
+    v31: String,
+    v32: String,
+    v33: String
+  ) derives Encoder.AsObject,
+        Decoder
+
+  object LongClass:
+    given Eq[LongClass] = Eq.fromUniversalEquals
+    given Arbitrary[LongClass] = Arbitrary {
+      for
+        s1 <- Arbitrary.arbitrary[String]
+        s2 <- Arbitrary.arbitrary[String]
+        s3 <- Arbitrary.arbitrary[String]
+        s4 <- Arbitrary.arbitrary[String]
+        s5 <- Arbitrary.arbitrary[String]
+        s6 <- Arbitrary.arbitrary[String]
+        s7 <- Arbitrary.arbitrary[String]
+        s8 <- Arbitrary.arbitrary[String]
+        s9 <- Arbitrary.arbitrary[String]
+        s10 <- Arbitrary.arbitrary[String]
+        s11 <- Arbitrary.arbitrary[String]
+        s12 <- Arbitrary.arbitrary[String]
+        s13 <- Arbitrary.arbitrary[String]
+        s14 <- Arbitrary.arbitrary[String]
+        s15 <- Arbitrary.arbitrary[String]
+        s16 <- Arbitrary.arbitrary[String]
+        s17 <- Arbitrary.arbitrary[String]
+        s18 <- Arbitrary.arbitrary[String]
+        s19 <- Arbitrary.arbitrary[String]
+        s20 <- Arbitrary.arbitrary[String]
+        s21 <- Arbitrary.arbitrary[String]
+        s22 <- Arbitrary.arbitrary[String]
+        s23 <- Arbitrary.arbitrary[String]
+        s24 <- Arbitrary.arbitrary[String]
+        s25 <- Arbitrary.arbitrary[String]
+        s26 <- Arbitrary.arbitrary[String]
+        s27 <- Arbitrary.arbitrary[String]
+        s28 <- Arbitrary.arbitrary[String]
+        s29 <- Arbitrary.arbitrary[String]
+        s30 <- Arbitrary.arbitrary[String]
+        s31 <- Arbitrary.arbitrary[String]
+        s32 <- Arbitrary.arbitrary[String]
+        s33 <- Arbitrary.arbitrary[String]
+      yield LongClass(
+        s1,
+        s2,
+        s3,
+        s4,
+        s5,
+        s6,
+        s7,
+        s8,
+        s9,
+        s10,
+        s11,
+        s12,
+        s13,
+        s14,
+        s15,
+        s16,
+        s17,
+        s18,
+        s19,
+        s20,
+        s21,
+        s22,
+        s23,
+        s24,
+        s25,
+        s26,
+        s27,
+        s28,
+        s29,
+        s30,
+        s31,
+        s32,
+        s33
+      )
+    }
+
+  enum LongEnum derives Encoder, Decoder:
+    case v1, v2, v3, v4, v5, v6, v7, v8, v9, v10,
+      v11, v12, v13, v14, v15, v16, v17, v18, v19, v20,
+      v21, v22, v23, v24, v25, v26, v27, v28, v29, v30,
+      v31, v32, v33
+
+  object LongEnum:
+    given Eq[LongEnum] = Eq.fromUniversalEquals
+    given Arbitrary[LongEnum] = Arbitrary(Gen.oneOf(LongEnum.values))
+
+  enum LongSum derives Encoder, Decoder:
+    case v1(str: String)
+    case v2(str: String)
+    case v3(str: String)
+    case v4(str: String)
+    case v5(str: String)
+    case v6(str: String)
+    case v7(str: String)
+    case v8(str: String)
+    case v9(str: String)
+    case v10(str: String)
+    case v11(str: String)
+    case v12(str: String)
+    case v13(str: String)
+    case v14(str: String)
+    case v15(str: String)
+    case v16(str: String)
+    case v17(str: String)
+    case v18(str: String)
+    case v19(str: String)
+    case v20(str: String)
+    case v21(str: String)
+    case v22(str: String)
+    case v23(str: String)
+    case v24(str: String)
+    case v25(str: String)
+    case v26(str: String)
+    case v27(str: String)
+    case v28(str: String)
+    case v29(str: String)
+    case v30(str: String)
+    case v31(str: String)
+    case v32(str: String)
+    case v33(str: String)
+
+  object LongSum:
+    given Eq[LongSum] = Eq.fromUniversalEquals
+    given Arbitrary[LongSum] = Arbitrary(
+      for
+        v <- Arbitrary.arbitrary[String]
+        res <- Gen.oneOf(
+          Seq(
+            v1(v),
+            v2(v),
+            v3(v),
+            v4(v),
+            v5(v),
+            v6(v),
+            v7(v),
+            v8(v),
+            v9(v),
+            v10(v),
+            v11(v),
+            v12(v),
+            v13(v),
+            v14(v),
+            v15(v),
+            v16(v),
+            v17(v),
+            v18(v),
+            v19(v),
+            v20(v),
+            v21(v),
+            v22(v),
+            v23(v),
+            v24(v),
+            v25(v),
+            v26(v),
+            v27(v),
+            v28(v),
+            v29(v),
+            v30(v),
+            v31(v),
+            v32(v),
+            v33(v)
+          )
+        )
+      yield res
+    )
 }
 
 class DerivesSuite extends CirceMunitSuite {
@@ -242,6 +443,9 @@ class DerivesSuite extends CirceMunitSuite {
   checkAll("Codec[ADTWithSubTraitExample]", CodecTests[ADTWithSubTraitExample].codec)
   checkAll("Codec[ProductWithTaggedMember] (#2135)", CodecTests[ProductWithTaggedMember].codec)
   checkAll("Codec[Outer]", CodecTests[Outer].codec)
+  checkAll("Codec[LongClass]", CodecTests[LongClass].codec)
+  checkAll("Codec[LongSum]", CodecTests[LongSum].codec)
+  checkAll("Codec[LongEnum]", CodecTests[LongEnum].codec)
 
   test("Nested sums should not be encoded redundantly") {
     val foo: ADTWithSubTraitExample = TheClass(0)

ikeyan/circe

commit 56f088f7d71e7139a8baa5633b46ebea5ccb7195

Fix regression introduced in #2341
The newly introduced classes in derivation package needs to be public
because of inline modifier. We need to be able to instantiate the
classes outside of the io.circe package.

Perhaps a different encoding of the problem would be needed, but this
fixes the initial problem.

modules/core/shared/src/main/scala-3/io/circe/Derivation.scala
@@ -18,8 +18,6 @@ package io.circe
 
 import scala.compiletime.constValue
 import scala.deriving.Mirror
-import Predef.genericArrayOps
-import cats.data.{ NonEmptyList, Validated }
 import io.circe.derivation.*
 
 @deprecated(since = "0.14.4")

modules/core/shared/src/main/scala-3/io/circe/derivation/package.scala
@@ -18,7 +18,7 @@ package io.circe.derivation
 
 import scala.compiletime.{ codeOf, constValue, erasedValue, error, summonFrom, summonInline }
 import scala.deriving.Mirror
-import io.circe.{ Codec, Decoder, Encoder }
+import io.circe.{ Decoder, Encoder }
 
 private[circe] inline final def summonLabels[T <: Tuple]: List[String] =
   loopUnrolledNoArg[String, T](
@@ -91,25 +91,25 @@ private[circe] inline final def loopUnrolled[A, Arg, T <: Tuple](f: Inliner[A, A
 private[circe] inline def loopUnrolledNoArg[A, T <: Tuple](f: Inliner[A, Unit]): List[A] =
   loopUnrolled[A, Unit, T](f, ())
 
-private[circe] trait Inliner[A, Arg]:
+sealed trait Inliner[A, Arg]:
   inline def apply[T](inline arg: Arg): A
 
-private[circe] object constString extends Inliner[String, Unit]:
+object constString extends Inliner[String, Unit]:
   inline def apply[T](inline arg: Unit): String = constValue[T].asInstanceOf[String]
 
-private[circe] class EncoderDeriveSum(using config: Configuration) extends Inliner[Encoder[_], Unit]:
+class EncoderDeriveSum(using config: Configuration) extends Inliner[Encoder[_], Unit]:
   inline def apply[T](inline arg: Unit): Encoder[?] = summonEncoder[T](true)
 
-private[circe] class EncoderNotDeriveSum(using config: Configuration) extends Inliner[Encoder[_], Unit]:
+class EncoderNotDeriveSum(using config: Configuration) extends Inliner[Encoder[_], Unit]:
   inline def apply[T](inline arg: Unit): Encoder[?] = summonEncoder[T](false)
 
-private[circe] class DecoderDeriveSum(using Configuration) extends Inliner[Decoder[_], Unit]:
+class DecoderDeriveSum(using Configuration) extends Inliner[Decoder[_], Unit]:
   inline def apply[T](inline arg: Unit): Decoder[?] = summonDecoder[T](true)
 
-private[circe] class DecoderNotDeriveSum(using Configuration) extends Inliner[Decoder[_], Unit]:
+class DecoderNotDeriveSum(using Configuration) extends Inliner[Decoder[_], Unit]:
   inline def apply[T](inline arg: Unit): Decoder[?] = summonDecoder[T](false)
 
-private[circe] class SummonSingleton[A] extends Inliner[A, Any]:
+class SummonSingleton[A] extends Inliner[A, Any]:
   inline def apply[T](inline typeName: Any): A =
     inline summonInline[Mirror.Of[T]] match
       case m: Mirror.Singleton => m.fromProduct(EmptyTuple).asInstanceOf[A]